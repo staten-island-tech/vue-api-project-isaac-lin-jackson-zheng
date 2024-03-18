dd1ed19c6943a436ebab25b7ae0b56c98ea62762
isaacl81 <isaaclin2007@gmail.com>
2024-03-18 11:06:00 -0400

    logged api data

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+  import { ref, onMounted } from 'vue'
+  const taxes = ref('')
+  async function getData() {
+    let res = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
+    let data = await res.json()
+    console.log(data)
+    taxes.value = data
+  };
+  onMounted(() => {
+    getData(taxes)
+  });
 </script>
 
 <template>
-
+  <h1>Title</h1>
 </template>
 
 <style scoped>
--- vite-project/src/views/Data02.vue
+++ vite-project/src/views/Data02.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script setup>
-
+  import { ref, onMounted } from 'vue'
+  const taxes = ref('')
+  async function getData() {
+    let res = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
+    let data = await res.json()
+    console.log(data)
+    taxes.value = data
+  };
+  onMounted(() => {
+    getData(taxes)
+  });
 </script>
 
 <style scoped>
