2e2e81db4a7ad18dbea58ed101c74746006825b7
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-03-21 00:30:20 -0400

    added a graph that maybe should would with data but still needs testing

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,21 +1,54 @@
-<script setup>
-  import { ref, onMounted } from 'vue'
-  const taxes = ref('')
-  async function getData() {
-    let res = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
-    let data = await res.json()
-    console.log(data)
-    taxes.value = data
-  };
-  onMounted(() => {
-    getData(taxes)
-  });
-</script>
-
 <template>
-  <h1>Title</h1>
+  <div id="graph">
+    <h1>THE IRS</h1>
+    <canvas id="taxbar"></canvas>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import Chart from 'chart.js/auto'
+import { ref, onMounted } from 'vue'
 
+const taxes = ref('')
+
+async function getdatas() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
+  let data = await response.json()
+  taxes.value = data
+  console.log(data)
+
+  const tax = data
+
+  const ctx = document.getElementById('taxbar')
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: tax.map((row) => row.nm),
+      datasets: [
+        {
+          label: 'year',
+          data: tax.map((row) => row.cnt),
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      indexAxis: 'y',
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+}
+onMounted(() => {
+  getdatas()
+})
+</script>
+
+<style scoped>
+#graph {
+  background-color: white;
+}
 </style>
