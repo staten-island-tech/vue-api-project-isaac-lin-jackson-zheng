5db794d17e0e967b043a2707a0c1983cb9a87c7f
jacksonz16 <jacksonz2390@gmail.com>
2024-03-28 09:08:49 -0400

    1

--- vite-project/src/views/BarGraph.vue
+++ vite-project/src/views/BarGraph.vue
@@ -1,6 +1,10 @@
 <template>
+  <label for="time-period">Time Period: </label>
+  <select name="time-period" id="select-time" v-model="selected" @change="getData()">
+    <option v-for="option in options" :value="option.value">{{ option.year }}</option>
+  </select>
+
   <div id="graph">
-    <h1>THE IRS</h1>
     <canvas id="taxbar"></canvas>
   </div>
 </template>
@@ -8,54 +12,93 @@
 <script setup>
 import Chart from 'chart.js/auto'
 import { ref, onMounted } from 'vue'
+import { sortData } from '../stores/sortBarData.js'
 
-const taxes = ref('')
+const options = ref([
+  { year: '2010s', value: { min: 2010, max: 2021 } },
+  { year: '2000s', value: { min: 2000, max: 2010 } },
+  { year: '1990s', value: { min: 1990, max: 2000 } },
+  { year: '1980s', value: { min: 1979, max: 1990 } },
+])
+let selected = ref('')
+const taxes = ref([])
+let chart = null
 
-async function getdatas() {
+async function getData() {
+  console.log(options)
+  
   let response = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
-  let data = await response.json()
-  taxes.value = data
-  console.log(data)
+  let rawData = await response.json()
+  taxes.value = rawData
 
-  function stringWithCommas() {
-  let stringWithCommas = row.data;
-let stringWithoutCommas = stringWithCommas.replace(',', '');
-console.log(stringWithoutCommas); 
+  const convert = string => parseInt(string.replace(/,/g, ''), 10);
+  const data = rawData.map(object => ({
+    ...object,
+    year: convert(object.year),
+    total_taxes: convert(object.total_taxes),
+    commercial_rent: convert(object.commercial_rent),
+    conveyance_of_real_property: convert(object.conveyance_of_real_property),
+    financial_corporation: convert(object.financial_corporation),
+    general_corporation: convert(object.general_corporation),
+    general_sales: convert(object.general_sales),
+    mortgage_recording: convert(object.mortgage_recording),
+    other_taxes: convert(object.other_taxes),
+    personal_income_general_fund_revenue: convert(object.personal_income_general_fund_revenue),
+    personal_income_total: convert(object.personal_income_total),
+    property: convert(object.property),
+    unincorporated_business_income: convert(object.unincorporated_business_income),
+  }));
+
+  // const decade = options.value.find(option => option.value.min === selected.value.min && option.value.max === selected.value.max)
+  // const tax = data.filter(data => data.year >= decade.value.min && data.year <= decade.value.max)
+
+  function displayData() {
+    if (selected === undefined) {
+      const tax = data.filter(data => data.year >= 2010 && data.year <= 2020)
+      return tax
+    } else if (selected.selectedIndex !== -1) {
+      const decade = options.value.find(option => option.value.min === selected.value.min && option.value.max === selected.value.max)
+      const tax = data.filter(data => data.year >= decade.value.min && data.year <= decade.value.max)
+      return tax
+    }
   }
-stringWithCommas.log
+  const tax = displayData()
+  console.log(tax)
+  console.log(selected)
 
-const tax = data.filter((data) => data.year > 1999 )
+  if (chart) {
+    chart.destroy()
+  }
 
   const ctx = document.getElementById('taxbar')
-  new Chart(ctx, {
-    type: 'bar',//this line changes the graph type you need to look like you are doing more and not carried by me so just changed it to "doughnut"
-    data: {
-      labels: tax.map((row) => row.year),
-      datasets: [
-        {
-          label: 'Total Tax',
-          data: tax.map((row) => row.year),//change the listing for the new value with out the comma with numbers
+  if (ctx) {
+    chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: tax.map(row => row.year),
+        datasets: [{
+          label: 'Total Revenue',
+          data: tax.map(row => row.total_taxes),
           borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      indexAxis: 'y',
-      scales: {
-        y: {
-          beginAtZero: true
+        }]
+      },
+      options: {
+        indexAxis: 'x',
+        scales: {
+          x: {
+            beginAtZero: true,
+            reverse: true
+          }
         }
       }
-    }
-  })
+    })
+  }
 }
 onMounted(() => {
-  getdatas()
+  getData()
 })
 </script>
 
 <style scoped>
-#graph {
-  background-color: white;
-}
+
 </style>
--- vite-project/src/views/DonutChart.vue
+++ vite-project/src/views/DonutChart.vue
@@ -1,6 +1,12 @@
 <template>
+  <label for="time-period"></label>
+  <select name="time-period" id="select-time">
+    <option min="2009" max="2020">2010s</option>
+    <option min="1999" max="2010">2000s</option>
+    <option min="1989" max="2000">1990s</option>
+    <option min="1979" max="1990">1980s</option>
+  </select>
   <div id="graph">
-    <h1>THE IRS</h1>
     <canvas id="taxbar"></canvas>
   </div>
 </template>
@@ -11,51 +17,71 @@ import { ref, onMounted } from 'vue'
 
 const taxes = ref('')
 
-async function getdatas() {
+async function getData() {
   let response = await fetch('https://data.cityofnewyork.us/resource/hdnu-nbrh.json')
-  let data = await response.json()
-  taxes.value = data
-  console.log(data)
+  let rawData = await response.json()
+  taxes.value = rawData
+  // console.log(rawData)
 
-  function stringWithCommas() {
-  let stringWithCommas = row.data;
-let stringWithoutCommas = stringWithCommas.replace(',', '');
-console.log(stringWithoutCommas); 
-  }
-stringWithCommas.log
+  const convert = string => parseInt(string.replace(/,/g, ''), 10);
+  const data = rawData.map(object => ({
+    ...object,
+    year: convert(object.year),
+    total_taxes: convert(object.total_taxes),
+    commercial_rent: convert(object.commercial_rent),
+    conveyance_of_real_property: convert(object.conveyance_of_real_property),
+    financial_corporation: convert(object.financial_corporation),
+    general_corporation: convert(object.general_corporation),
+    general_sales: convert(object.general_sales),
+    mortgage_recording: convert(object.mortgage_recording),
+    other_taxes: convert(object.other_taxes),
+    personal_income_general_fund_revenue: convert(object.personal_income_general_fund_revenue),
+    personal_income_total: convert(object.personal_income_total),
+    property: convert(object.property),
+    unincorporated_business_income: convert(object.unincorporated_business_income),
+  }));
+  console.log(data);
 
-const tax = data.filter((data) => data.year > 1999 )
-
+  let minimum = 2009
+  let maximum = 2020
+  document.getElementById("select-time").addEventListener("change", function() {
+    const selectedOption = this.options[this.selectedIndex];
+    minimum = selectedOption.getAttribute("min");
+    maximum = selectedOption.getAttribute("max");
+    console.log(minimum)
+    console.log(maximum)
+  });
+  
+  const tax = data.filter((data) => data.year > minimum && data.year < maximum)
   const ctx = document.getElementById('taxbar')
   new Chart(ctx, {
-    type: 'doughnut',//this line changes the graph type you need to look like you are doing more and not carried by me so just changed it to "doughnut"
+    type: 'doughnut',
     data: {
       labels: tax.map((row) => row.year),
       datasets: [
         {
-          label: 'Total Tax',
-          data: tax.map((row) => row.year),//change the listing for the new value with out the comma with numbers
+          label: 'property Revenue',
+          data: tax.map((row) => row.property),
           borderWidth: 1
         }
       ]
     },
     options: {
-      indexAxis: 'y',
+      indexAxis: 'x',
       scales: {
-        y: {
-          beginAtZero: true
+        x: {
+          beginAtZero: true,
+          reverse: true,
         }
       }
     }
   })
 }
 onMounted(() => {
-  getdatas()
+  getData()
 })
 </script>
 
 <style scoped>
-#graph {
-  background-color: white;
-}
+
 </style>
