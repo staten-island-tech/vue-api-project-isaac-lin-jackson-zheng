f2d828b5e2a023d1a263bc6388d799f8a994c8c9
isaacl81 <isaaclin2007@gmail.com>
2024-03-31 11:04:37 -0400

    complete

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,8 +3,8 @@
     <h1>New York City Tax Revenue</h1>
   </div>
   <nav>
-    <RouterLink to="/" class="link">total taxes</RouterLink>
-    <RouterLink to="/doughnut" class="link">propertyTax</RouterLink>
+    <RouterLink to="/" class="link">Total Revenues</RouterLink>
+    <RouterLink to="/pie" class="link">Revenue Sources</RouterLink>
   </nav>
   <div id="data">
     <RouterView/>
@@ -16,33 +16,34 @@
 </script>
 
 <style scoped>
-#header {
-  margin: 2vw 0px 2vw 0px;
-  padding: 2vw;
-  display: flex;
-  justify-content: center;
-  color: white;
-  background-color: #003884;
-  border: solid #ff6600 thick;
-  border-left-style: none; border-right-style: none;
-}
-nav {
-  display: flex;
-  margin-bottom: 2vw;
-  gap: 10vw;
-  flex-direction: row;
-  justify-content: center;
-}
-.link {
-  padding: 12px;
-  background-color: #003884;
-  border-radius: 3px;
-  color: white;
-  font-size: larger;
-  text-decoration: none;
-}
-#data {
-  margin: auto;
-  width: 75%;
-}
+  #header {
+    margin: 1vw 0px 2vw 0px;
+    padding: 2vw;
+    display: flex;
+    justify-content: center;
+    color: white;
+    background-color: #003884;
+    border: solid #ff6600 thick;
+    border-left-style: none; border-right-style: none;
+  }
+  nav {
+    display: flex;
+    margin-bottom: 2vw;
+    gap: 10vw;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .link {
+    padding: 12px;
+    background-color: #003884;
+    border-radius: 3px;
+    color: white;
+    font-size: larger;
+    text-decoration: none;
+  }
+  #data {
+    margin: auto;
+    padding-bottom: 2vw;
+    width: 75%;
+  }
 </style>
--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import BarGraph from '@/views/BarGraph.vue'
-import DonutChart from '@/views/DonutChart.vue'
+import PieChart from '@/views/PieChart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +11,12 @@ const router = createRouter({
       component: BarGraph
     },
     {
-      path: '/doughnut',
-      name: 'doughnut',
+      path: '/pie',
+      name: 'pie',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: DonutChart
+      component: PieChart
     }
   ]
 })
