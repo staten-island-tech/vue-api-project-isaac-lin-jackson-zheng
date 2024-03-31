55062feb4d5a74222eebdd9eeba213cdbf752ad7
isaacl81 <isaaclin2007@gmail.com>
2024-03-31 11:19:30 -0400

    .

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import BarGraph from '@/views/BarGraph.vue'
+import PieChart from '@/views/PieChart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'bar',
+      component: BarGraph
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/pie',
+      name: 'pie',
+      component: PieChart
     }
   ]
 })
