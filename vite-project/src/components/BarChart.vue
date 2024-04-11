f98459803f4b6faf70bc867079edb8ebc6562cbb
jacksonz16 <jacksonz2390@gmail.com>
2024-04-11 09:23:35 -0400

    1

--- vite-project/src/components/BarChart.vue
+++ vite-project/src/components/BarChart.vue
@@ -7,25 +7,58 @@
   </template>
   
   <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  // import { Bar } from 'vue-chartjs'
+  // import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
   
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  // ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'February', 'March' ],
-          datasets: [ { data: [40, 20, 12] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
-    }
-  }
-  </script>
+  // export default {
+  //   name: 'BarChart',
+  //   components: { Bar },
+  //   data() {
+  //     return {
+  //       chartData: {
+  //         labels: [ 'January', 'February', 'March' ],
+  //         datasets: [ { data: [40, 20, 12] } ]
+  //       },
+  //       chartOptions: {
+  //         responsive: true
+  //       }
+  //     }
+  //   }
+  // }
+  // </script>
+  // <template>
+  //   <div class="container">
+  //     <Bar v-if="loaded" :data="chartData" />
+  //   </div>
+  // </template>
+  
+  // <script>
+  // import { Bar } from 'vue-chartjs'
+  // import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  
+  // ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  
+  // export default {
+  //   name: 'BarChart',
+  //   components: { Bar },
+  //   data: () => ({
+  //     loaded: false,
+  //     chartData: null
+  //   }),
+  //   async mounted () {
+  //     this.loaded = false
+  
+  //     try {
+  //       const { userlist } = await fetch('/api/userlist')
+  //       this.chartdata = userlist
+  
+  //       this.loaded = true
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   }
+  // }
+  // </script>
   
