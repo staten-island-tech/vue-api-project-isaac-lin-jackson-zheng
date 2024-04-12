d9c254d78dfdc87ba93ac3474ccf0a86c485c54b
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-04-12 05:59:20 -0400

    1

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -5,11 +5,9 @@ import BarChart from "../views/About.vue";
 const routes = [
   { path: "/doughnut", name: "Doughnut", component: BarChart },
   { path: "/bar", name: "Bar", component: ActualBar },
- 
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
 export default router;
--- vite-project/src/views/About.vue
+++ vite-project/src/views/About.vue
@@ -1,15 +1,103 @@
+<script setup></script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="chart">
+    <Doughnut
+      v-if="load"
+      id="my-chart"
+      :options="chartOptions"
+      :data="chartData"
+    />
   </div>
 </template>
+<script>
+import { Doughnut } from "vue-chartjs";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { ref, reactive } from "vue";
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default {
+  name: "BarChart", // renaming it crashes the page lol
+  components: { Doughnut },
+  props: {},
+  data() {
+    return {
+      load: false,
+      chartData: null,
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+
+  async mounted() {
+    try {
+      const response = await fetch(
+        "https://data.cityofnewyork.us/resource/hdnu-nbrh.json"
+      );
+      const years = await response.json();
+      const filtered = years.filter((x) => {
+        return x.year !== undefined;
+      });
+      const year1 = filtered.filter((e) => {
+        return e.year.includes(1);
+      });
+      const year2 = filtered.filter((e) => {
+        return e.year.includes(2);
+      });
+      const year3 = filtered.filter((e) => {
+        return e.year.includes(3);
+      });
+      const year4 = filtered.filter((e) => {
+        return e.year.includes(4);
+      });
+      const year5 = filtered.filter((e) => {
+        return e.year.includes(50);
+      });
+
+
+      this.chartData = {
+        labels: ["1", "2", "3", "4", "5",],
+        datasets: [
+          {
+            label: "Number of uses of number",
+            backgroundColor: ["#FF0000"],
+            data: [
+              year1.length,
+              year2.length,
+              year3.length,
+              year4.length,
+              year5.length,
+
+            ],
+          },
+        ],
+      };
+      this.load = true;
+      console.log(this.load);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
+</script>
+
+<style scoped>
+.present {
+  height: 20rem;
+  width: 20rem;
+  border: solid black 1rem;
+}
+.chart {
+  background: white;
+  height: 30rem;
+  width: 30rem;
+  border: solid white 1rem;
+  border-radius: 2rem;
+}
+.chart {
+  margin: auto;
+  align-items: center;
 }
 </style>
